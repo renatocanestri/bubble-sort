b7b448946fc5782bf8d8a9c4130810b6c45980c9
Renato Canestri <137238517+renatocanestri@users.noreply.github.com>
2023-09-18 17:28:34 -0300

    Upload first version

---
+++ script.rb
@@ -0,0 +1,19 @@
+def bubble_sort(array)
+ swapped = true
+   while swapped
+     swapped = false
+     array.each_with_index do |element, index|
+      if (index > 0)
+        if (array[index - 1] > array[index])
+          temp_element = array[index - 1]
+          array[index - 1] = array[index]
+          array[index] = temp_element
+          swapped = true
+        end
+      end
+    end
+  end
+  return array
+end
+
+p bubble_sort([4,3,78,2,0,2])
